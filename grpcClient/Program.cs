cdfa9e924941e6d08b16ca50b32f788442b30f2f
Fatih <fath.varl@gmail.com>
2021-07-18 16:20:28 +0300

    first init

---
+++ grpcClient/Program.cs
@@ -0,0 +1,108 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace grpcClient
+{
+    class Program
+    {
+        static Grpc.Core.ChannelBase channel = GrpcChannel.ForAddress("https://localhost:5001");
+        static Message.MessageClient messageClient = new Message.MessageClient(channel);
+
+        static async Task Main(string[] args)
+        {
+
+            //Unary
+            await UnaryStreaming();
+
+            //Server Streaming
+            //await ServerStreaming();
+
+            //ClientStreaming
+            //await ClientStreaming();
+
+            //Bi-Directional Streaming
+            //await BiDirectionalStreaming();
+
+            Console.ReadKey();
+        }
+
+        static async Task UnaryStreaming()
+        {
+            MessageResponse response = await messageClient.SendMessageUnaryAsync(new MessageRequest
+            {
+                Message = "Hello",
+                Name = "Fatih"
+            });
+            Console.WriteLine(response.Message);
+        }
+
+        static async Task ServerStreaming()
+        {
+            var response = messageClient.SendMessageServerStreaming(new MessageRequest
+            {
+                Message = "Hello",
+                Name = "Fatih"
+            });
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            while (await response.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            {
+                Console.WriteLine(response.ResponseStream.Current.Message);
+            }
+
+        }
+
+        static async Task ClientStreaming()
+        {
+            var request = messageClient.SendMessageClientStreaming();
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                await request.RequestStream.WriteAsync(new MessageRequest
+                {
+                    Name = "Fatih",
+                    Message = "Hello " + i
+                });
+            }
+            await request.RequestStream.CompleteAsync(); //Stream data finished
+
+            var response = await request.ResponseAsync;
+            Console.WriteLine(response.Message);
+        }
+
+        static async Task BiDirectionalStreaming()
+        {
+
+            var request = messageClient.SendMessageBiDirectionalStreaming();
+
+            var requestTask = Task.Run(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    await request.RequestStream.WriteAsync(new MessageRequest
+                    {
+                        Name = "Fatih",
+                        Message = "Hello " + i
+                    });
+                }
+            });
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            while (await request.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            {
+
+                Console.WriteLine(request.ResponseStream.Current.Message);
+            }
+            await request.RequestStream.CompleteAsync();
+            await requestTask;
+
+
+
+        }
+    }
+}
+
+
---
+++ grpcServer/Services/MessageService.cs
@@ -0,0 +1,81 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace grpcServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        public override Task<MessageResponse> SendMessageUnary(MessageRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Type : Unary | Message : {request.Message} | Name : {request.Name}");
+            return Task.FromResult(new MessageResponse
+            {
+                Message = "Message received successfully!"
+            });
+        }
+
+        public override async Task SendMessageServerStreaming(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"Type : ServerStreaming | Message : {request.Message} | Name : {request.Name}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(200);
+                await responseStream.WriteAsync(new MessageResponse
+                {
+                    Message = "Hello " + i
+                });
+            }
+        }
+
+        public override async Task<MessageResponse> SendMessageClientStreaming(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext(context.CancellationToken))
+            {
+                Console.WriteLine($"Type : ClientStreaming | Message : {requestStream.Current.Message} | Name : {requestStream.Current.Name}");
+            }
+            return new MessageResponse
+            {
+                Message = "Message has stream data!"
+            };
+        }
+
+        public override async Task SendMessageBiDirectionalStreaming(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            try
+            {
+                if(!await requestStream.MoveNext())
+                {
+                    return;
+                }
+
+                var requestTask = Task.Run(async () =>
+                {
+                    while (await requestStream.MoveNext(context.CancellationToken))
+                    {
+                        Console.WriteLine($"Type : BiDirectionalStreaming | Message : {requestStream.Current.Message} | Name : {requestStream.Current.Name}");
+                    }
+                });
+
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    await responseStream.WriteAsync(new MessageResponse
+                    {
+                        Message = "Message " + i
+                    });
+                }
+
+                await requestTask;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Client was closed");
+                
+            }
+        }
+    }
+}
